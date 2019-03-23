7a88528da5fa8bac2b2776eef0dd4d7581af5879
Francis Kip <gwadafrank@gmail.com>
2019-03-23 22:04:18 +0300

    feat(users): set up docker configurations

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask, jsonify
 from .config import app_config
 
 # instantiate the app
 app = Flask(__name__)
 
+
 # set config
-app.config.from_object(app_config.get('dev'))
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
